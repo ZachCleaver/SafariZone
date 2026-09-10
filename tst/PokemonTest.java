6bda9e2492b186aee18371584e59e071df072c3a
ZachCleaver <zucleaver@gmail.com>
2026-09-10 09:04:59 -0500

    Added basic unit test

---
+++ tst/PokemonTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PokemonTest {
+
+    @Test
+    public void test_renamePokemon() {
+        Pokemon pokemon = new Pokemon("Pikachu");
+        pokemon.renamePokemon("Garble");
+        assertEquals("Garble", pokemon.getName());
+    }
+}
